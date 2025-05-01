769134bf49b449204591a06e26d8cfba0f771907
dgich <dgich@samokat.ru>
2025-05-01 13:49:21 +0300

    Завершено описание модели в test_py, коммит перед пересборкой приложения на новую модель

--- app/tests_run.py
+++ app/tests_run.py
@@ -1,13 +1,14 @@
 from pymongo import MongoClient
 from pymongo.database import Database
+from pymongo.cursor import Cursor
 from enum import Enum
 from services import get_logger
 from bson import ObjectId
 from typing import Union, List, Optional
 from dataclasses import dataclass, field
-
-import json
-import jsonschema
+from jsonschema import validate, ValidationError
+from datetime import datetime
+from json import dumps
 
 # Соответствие bsonType -> type
 BSON_TO_JSON_TYPE = {
@@ -46,20 +47,26 @@ class ModelField:
     type: Union['ModelFieldTypes', List['ModelFieldTypes']]
     source: ModelFieldSources = ModelFieldSources.MODEL
     name: Optional[str] = None
-    val: Optional[any] = None
+    default_val: Optional[any] = None
     is_required: bool = False
     validation_rules: dict = field(default_factory=dict)
 
     def __repr__(self):
-        return f"<{self.__class__.__name__}. Name={self.name}, Type={self.type}, Val={self.val}>"
+        return f"<{self.__class__.__name__}. Name={self.name}, Type={self.type}, Val={self.default_val}>"
 
     def __str__(self):
-        return f"<{self.__class__.__name__}. Name={self.name}, Type={self.type}, Val={self.val}>"
+        return f"<{self.__class__.__name__}. Name={self.name}, Type={self.type}, Val={self.default_val}>"
     
+    def __eq__(self, other):
+        return isinstance(other, ModelField) and self.name == other.name and self.type == other.type
+
+    def __hash__(self):
+        return hash((self.name, str(self.type)))
+
     def __post_init__(self):
         if not isinstance(self.type, (ModelFieldTypes, list)):
             raise TypeError(f"Invalid type: {self.type}")
-
+        
 class ModelFabric(type):
     def __new__(cls, name, bases, dct):
         new_cls = super().__new__(cls, name, bases, dct)
@@ -69,7 +76,6 @@ class ModelFabric(type):
         
         return new_cls
 
-    
 class ModelMeta(metaclass=ModelFabric):
 
     _schema = None
@@ -93,7 +99,7 @@ class ModelMeta(metaclass=ModelFabric):
         self.jsonschema = self.__get_schema()
         self.fields = self.__set_fields_list()
 
-    def __set_fields_list(self):
+    def __set_fields_list(self) -> list:
         """Собираем все поля в мету модели"""
         model_fields = self.__collect_model_fields()
         mongo_fields = self.__collect_mongo_fields()
@@ -217,6 +223,144 @@ class ModelMeta(metaclass=ModelFabric):
 
     def __str__(self):
         return f"<{self.__class__.__name__}. Collection={self.collection}, Fields = {self.fields}>"
+    
+class ModelFieldValueTransformer:
+
+    @staticmethod
+    def __handle_special_types(val, t):
+        """
+        Обрабатывает специфические типы данных, которые требуют приведения или десериализации
+        вручную, перед валидацией по jsonschema.
+
+        Используется как предварительная стадия трансформации до основного кастинга типов.
+
+        Args:
+            val: Значение поля, которое нужно обработать.
+            t (ModelFieldTypes): Целевой тип из перечисления ModelFieldTypes.
+
+        Returns:
+            Приведенное значение, если удалось выполнить конвертацию.
+            None — если тип не требует спец. обработки или приведение не выполнено.
+
+        Обрабатываемые типы:
+        - OBJECTID: Преобразует строку или уже готовый ObjectId в ObjectId
+        - DATE: Преобразует ISO-строку в datetime.datetime (aware)
+        """
+
+        # Обработка ObjectId: либо возвращаем как есть, либо преобразуем из строки
+        if t == ModelFieldTypes.OBJECTID:
+            return val if isinstance(val, ObjectId) else ObjectId(val)
+        
+        # Обработка даты: если уже datetime, вернуть как есть, иначе распарсить ISO 8601 строку
+        if t == ModelFieldTypes.DATE:
+            return val if isinstance(val, datetime) else datetime.fromisoformat(val.replace("Z", "+00:00"))
+        
+        # Для остальных типов ничего не делаем
+        return None
+
+    @staticmethod
+    def transform(val, field_type, validation_rules=None, logger=None):
+        """
+        Преобразует и валидирует значение поля на основе допустимых типов и правил валидации.
+
+        Эта функция обрабатывает случаи, когда поле может иметь один или несколько допустимых типов 
+        (например, через `anyOf` в jsonschema), и пытается привести значение к первому подходящему типу.
+
+        Аргументы:
+            val: Значение, которое нужно привести.
+            field_type: Один или список типов из ModelFieldTypes.
+            validation_rules (dict): Правила валидации JSON Schema (pattern, minLength и т.д.)
+            logger (logging.Logger): Логгер для фиксации ошибок, если передан.
+
+        Возвращает:
+            Приведённое значение, если один из типов прошёл валидацию и преобразование,
+            иначе — оригинальное значение.
+        """
+        if val is None:
+            return None
+
+         # Приведение к списку (если один тип — оборачиваем в список)
+        types = field_type if isinstance(field_type, list) else [field_type]
+
+         # Пробуем по очереди все возможные типы
+        for t in types:
+            try:
+                # Если требуется специальная обработка (DATE, OBJECTID)
+                val = ModelFieldValueTransformer.__handle_special_types(val, t) or val
+                
+                # Формируем схему и добавляем валидаторы
+                schema = {"type": t.value}
+                if validation_rules:
+                    schema.update(validation_rules)
+
+                # Валидация по jsonschema
+                validate(instance=val, schema=schema)
+
+                # Если прошло валидацию — возвращаем приведённое значение
+                return ModelFieldValueTransformer.__cast(val, t)
+
+            except Exception as e:
+                if logger:
+                    logger.warning(f"[Transform] Не удалось преобразовать '{val}' в {t}: {e}")
+
+         # Если не удалось привести ни к одному типу — логируем и возвращаем оригинал
+        if logger:
+            logger.warning(f"[Transform] Оставлено оригинальное значение '{val}' (не найдено подходящего типа)")
+
+        return val
+
+    @staticmethod
+    def __cast(val, t: ModelFieldTypes):
+        if t == ModelFieldTypes.STRING:
+            return str(val)
+        if t == ModelFieldTypes.INT:
+            return int(val)
+        if t == ModelFieldTypes.NUMBER:
+            return float(val)
+        if t == ModelFieldTypes.BOOLEAN:
+            return bool(val)
+        if t == ModelFieldTypes.ARRAY:
+            return list(val)
+        if t == ModelFieldTypes.OBJECT:
+            return dict(val)
+        return val  
+
+class ModelMetaClass(metaclass=ModelFabric):
+
+    def __init__(self, doc:dict, fields:list):
+        self.__set_attributes(doc, fields)
+
+    def __set_attributes(self, doc: dict, fields: list):
+        # TBD Добавить получение динамических значений из вызов функций внутри модели (пока такое не надо)
+
+        for field in fields:
+            key = field.name
+            val = doc.get(key) or field.default_val # Пытаемся получить значение из БД, если его нет, ставим default_val из ModelField
+            val = self.__transform_field_value_type(val, field.type, field.validation_rules)
+            setattr(self, field.name, val)
+
+    def __transform_field_value_type(self, val, field_type, validation_rules):
+        """
+        Приводит значение к одному из допустимых типов внутри jsonschema
+        """
+
+        return ModelFieldValueTransformer.transform(val, field_type, validation_rules, logger=self._logger)
+
+    # Напишу валидацию по правилам из схемы, если надо будет
+    def __validate_field_value(self):
+        pass
+
+    def __attrs_list(self):
+        return ", ".join(f"{k}: {v!r}" for k, v in self.__dict__.items())
+
+    def __repr__(self):
+        return f"Object=<{self.__class__.__name__}. Attrs=[{self.__attrs_list()}]>"
+
+    def __str__(self):
+        return f"Object=<{self.__class__.__name__}. Attrs=[{self.__attrs_list()}]>"
+    
+    def to_dict(self):
+        return self.__dict__.copy()
 
 class QuerySet:
     def __init__(self, model_name=None, data=None):
@@ -232,15 +376,24 @@ class QuerySet:
     def append(self, obj):
         self._data.append(obj)
 
-    def len(self):
+    def __len__(self):
         return len(self._data)
 
     def to_list(self):
         return list(self._data)
+    
+    def first(self):
+        return self._data[0] if self._data else None
+    
+    def to_dict_list(self):
+        return [obj.to_dict() for obj in self._data]
+    
+    def to_json(self, **kwargs):
+        return dumps(self.to_dict_list(), default=str, ensure_ascii=False, **kwargs)
 
     def __repr__(self):
-        return f"<QuerySet Model={self.model_name}, Size={self.len()}>"
-
+        return f"<QuerySet Model={self.model_name}, Size={self.__len__()}>"
+    
 class ModelBase(metaclass=ModelFabric):
 
     # Базовые атрибуты при инициализации экземпляра класса
@@ -252,6 +405,7 @@ class ModelBase(metaclass=ModelFabric):
 
     def __init__(self, db:Database):
         self._db = db
+        self._cache = None
         self.meta = ModelMeta(
             db=self._db,
             collection=self._collection,
@@ -262,52 +416,52 @@ class ModelBase(metaclass=ModelFabric):
     def __get_model_fields(self) -> list:
         fields = []
 
-        # Перебираем все атрибуты класса
-        for key in dir(self):
-
-            # Убираем методы и служебние атрибуты, которые начинаются с _
-            if not key.startswith('_') and not callable(getattr(self, key)):
-                
-                field_candidate = getattr(self, key)
-
-                # Не обрабатываем атрибуты, которые не ModelField
-                if not isinstance(field_candidate, ModelField):
-                    self._logger.warning(f"Атрибут '{key}' не является экземпляром ModelField")
-                    continue
-                
-                # Добавляем атрибут в модель
+        for key, value in self.__class__.__dict__.items():
+            if isinstance(value, ModelField):
                 fields.append(ModelField(
                     name=key,
-                    type=field_candidate.type,
-                    val=field_candidate.val,
-                    source=ModelFieldSources.MODEL)
-                    )
+                    type=value.type,
+                    default_val=value.default_val,
+                    source=ModelFieldSources.MODEL
+                ))
+            else:
+                self._logger.warning(f"Атрибут '{key}' не является экземпляром ModelField")
 
         return fields
+
+    def __transform_mongo_data_to_model(self, data:Cursor) -> QuerySet:
+
+        documents = list(data)
+        MetaModelObject = type(self._model, (ModelMetaClass, ), {})
+        ret = QuerySet([])
+
+        # Возвращаем пустой QuerySet если запрос не вернул данных
+        if not documents:
+            return ret
+        
+        # Преобразуем словари в динамический объект, описанный внутри класса ModelMetaClass
+        # Имя класса берется из self._model
+        for doc in documents:
+            ret.append(MetaModelObject(doc=doc, fields=self.meta.fields))
+        
+        return ret
     
-    def __transform_mongo_data_to_model(self):
-        pass
+    def get_all(self) -> QuerySet:
 
-    def __validate_mongo_data(self):
-        pass
-    
-    def all(self):
-        pass
+        data = self._db[self._collection].find()
 
-    def get_one(self):
-        pass
+        if self._cache:
+            return self._cache
+        self._cache = self.__transform_mongo_data_to_model(data)
+        return self._cache
 
-    def get_many(self):
-        pass
-                                                        
-    def to_dict(self):
-        pass
+    def find_one(self, query:dict) -> QuerySet:
+        data = [self._db[self._collection].find_one(query)]
+        return self.__transform_mongo_data_to_model(data)
 
-    def to_json(self):
-        pass
-
-
-        pass
+    def find_many(self, query:dict):
+        data = self._db[self._collection].find(query)
+        return self.__transform_mongo_data_to_model(data)
 
 class Pages(ModelBase):
 
@@ -315,7 +469,7 @@ class Pages(ModelBase):
     _model = "Page"
 
     # Определил 3 поля внутри модели длч тестирования
-    new_field_one = ModelField(type=ModelFieldTypes.STRING, val="test") # Тут заполнение константой
+    new_field_one = ModelField(type=ModelFieldTypes.STRING, default_val="test") # Тут заполнение константой
     new_field_three = ModelField(type=ModelFieldTypes.STRING)
     new_field_four = ModelField(type=ModelFieldTypes.INT) # А тут будет динамическа заполняться
 
@@ -326,22 +480,13 @@ db = MongoClient("mongodb://localhost:27017")["docspace"]
 _obj = Pages(db=db)
 
 print ("")
-print (type(_obj))
-print ()
-# print (dir(_obj))
-# _obj = type("ClassName1", (MetaClass,), {})("val1", "val2")
+print (type(_obj), end="\n\n")
+# print (type(_obj.all()))
+_f = _obj.find_many({'order': 1})
+print (type(_f))
 
-# for field in _obj.meta.fields:
-#     print (f"Name = {field.name}, type={field.type}, source={field.source}, val={field.val}, is_required={field.is_required}, rules={field.validation_rules}")
+for doc in _f:
+    print (doc, end="\n\n")
 
-
-_tst = QuerySet("Test", [n for n in range(5)])
-print(_tst)
-
-for item in _tst:
-    print (item)
-
-_tst.append(5)
-print(_tst)
-
-print(_tst.to_list())
+print (f"Type = {type(_f.to_dict_list())}", _f.to_dict_list(), end="\n\n")
+print (f"Type = {type(_f.to_json())}", _f.to_json(), end="\n\n")
